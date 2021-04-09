2bff2993d74e7b2886098b5c053755a9e8b32d59
Ben Crowell <notreal@fakewillnotwork.invalid>
2021-04-09 11:28:49 -0700

    starting work

--- rashomon.rb
+++ rashomon.rb
@@ -4,16 +4,35 @@ require 'optparse'
 
 def main()
   options = opts()
-  data_dir = "raw"
+  raw_dir = "raw"
+  cache_dir = "cache"
 
   if ARGV.length<1 then die("supply an argument") end
   file = ARGV[0]
+
+  do_preprocess(file,raw_dir,cache_dir)
 
 end
 
 def die(message)
   $stderr.print message,"\n"
   exit(-1)
+end
+
+def do_preprocess(file,raw_dir,cache_dir)
+  raw = File.join(raw_dir,file+".txt")
+  cached = File.join(cache_dir,file,".json")
+  if not FileTest.exist?(raw) then die("file #{raw} not found") end
+  if FileTest.exist?(cached) and File.mtime(cached)>File.mtime(raw) then return end
+  t = slurp_file(raw).unicode_normalize
+  t.gsub!(/\r\n/,"\n") # crlf to unix newline
+  # clean up whitespace in and around newlines:
+    t.gsub!(/\n\s+\n/,"\n\n")
+    t.gsub!(/\n[ \t]+/,"\n")
+    t.gsub!(/[ \t]+\n/,"\n")
+  t.split(/\n\n/) { |paragraph|
+    print paragraph,"\n\n\n"
+  }
 end
 
 def opts()
@@ -27,6 +46,25 @@ def opts()
     end
   end.parse!
   return options
+end
+
+# returns contents or nil on error; for more detailed error reporting, see slurp_file_with_detailed_error_reporting()
+def slurp_file(file)
+  x = slurp_file_with_detailed_error_reporting(file)
+  return x[0]
+end
+
+# returns [contents,nil] normally [nil,error message] otherwise
+def slurp_file_with_detailed_error_reporting(file)
+  begin
+    File.open(file,'r') { |f|
+      t = f.gets(nil) # nil means read whole file
+      if t.nil? then t='' end # gets returns nil at EOF, which means it returns nil if file is empty
+      return [t,nil]
+    }
+  rescue
+    return [nil,"Error opening file #{file} for input: #{$!}."]
+  end
 end
 
 main()
